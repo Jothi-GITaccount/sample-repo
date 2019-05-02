785c908aa230e476f02d62006708f28cccd7298b
Jothipriya karunanithi <jkarunanithi@vmware.com>
2019-05-02 12:22:23 +0530

    commit

--- sample1/src/simpleprgm/tristar.java
+++ sample1/src/simpleprgm/tristar.java
@@ -4,7 +4,7 @@ public class tristar {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//text added 12 test comments
+//commit
 		int i,j;
 		for(i=1; i<=6; i++)
 		{
